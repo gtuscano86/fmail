2cabbe476c0b0c35e8b44c4b17b3dcb31bc4bd50
Gabby Tuscano <57541929+gtuscano86@users.noreply.github.com>
2020-02-03 16:45:40 -0500

    Add files via upload

---
+++ EmailClassLibrary/ClassEmail.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace EmailClassLibrary
+{
+    public class ClassEmail
+    {
+        private String sender;
+        private String receiver;
+        private String subject;
+        private String content;
+        private DateTime createdTime;
+        private String tag;
+
+        public ClassEmail()
+        {
+
+        }
+
+        public String Sender
+        {
+            get { return sender; }
+            set { sender = value; }
+        }
+
+        public String Receiver
+        {
+            get { return receiver; }
+            set { receiver = value; }
+        }
+
+        public String Subject
+        {
+            get { return subject; }
+            set { subject = value; }
+        }
+
+        public String Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+        public DateTime CreatedTime
+        {
+            get { return createdTime; }
+            set { createdTime = value; }
+        }
+
+        public String Tag
+        {
+            get { return tag; }
+            set { tag = value; }
+        }
+
+
+    }
+}
---
+++ EmailClassLibrary/User.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace EmailClassLibrary
+{
+    public class User
+    {
+        private String name;
+        private String address;
+        private String phoneNumber;
+        private String newEmail;
+        private String contactEmail;
+        private String avatar;
+        private String password;
+        private String active;
+        private String type;
+
+        public User()
+        {
+
+        }
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public String Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public String Phone
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        public String NewEmail
+        {
+            get { return newEmail; }
+            set { newEmail = value; }
+        }
+
+        public String ContactEmail
+        {
+            get { return contactEmail; }
+            set { contactEmail = value; }
+        }
+
+        public String Avatar
+        {
+            get { return avatar; }
+            set { avatar = value; }
+        }
+
+        public String Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public String Active
+        {
+            get { return active; }
+            set { password = value; }
+        }
+
+        public String Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+    }
+}
